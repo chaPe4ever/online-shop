383ce2f7e80a0dbb31bd70fc634245c50b782bd9
Petros Chatzipetrou <petro.chatzipetrou@gmail.com>
2025-11-16 16:57:20 +0100

    Add root persistor

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 import { Provider } from 'react-redux';
-import { store } from './store/store.js';
+import { store, persistor } from './store/store.js';
 import { BrowserRouter } from 'react-router';
 import { Toaster } from './components/ui/sonner';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Spinner } from './components/ui/spinner';
+import { PersistGate } from 'redux-persist/integration/react';
 
 // TanStack Query client required setting up the reactQueries
 const queryClient = new QueryClient({
@@ -23,10 +25,12 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
       <QueryClientProvider client={queryClient}>
-        <BrowserRouter>
-          <App />
-          <Toaster />
-        </BrowserRouter>
+        <PersistGate loading={<Spinner />} persistor={persistor}>
+          <BrowserRouter>
+            <App />
+            <Toaster />
+          </BrowserRouter>
+        </PersistGate>
       </QueryClientProvider>
     </Provider>
   </StrictMode>
--- src/store/store.js
+++ src/store/store.js
@@ -1,12 +1,41 @@
 import logger from 'redux-logger';
-
-import { rootReducer } from './root-reducer';
 import { configureStore } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // localStorage
+import { rootReducer } from './root-reducer';
+
+// Persist configuration
+const persistConfig = {
+  key: 'root',
+  storage,
+  // Whitelist specific reducers to persist
+  whitelist: ['cart'], // Only cart and auth will be persisted
+  // Blacklist reducers you don't want to persist
+  // blacklist: ['someReducer'],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const middleWares = import.meta.env.DEV ? [logger] : [];
 
 export const store = configureStore({
-  reducer: rootReducer,
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(middleWares),
+    getDefaultMiddleware({
+      serializableCheck: {
+        // Ignore redux-persist actions
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }).concat(middleWares),
 });
+
+export const persistor = persistStore(store);
